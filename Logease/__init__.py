6bdd1012654400788276524bffd3f11cb08ad1b8
Hinjikani <pascalhanif@gmail.com>
2024-12-30 08:06:05 +0700

    Main package configuration

--- Logease/__init__.py
+++ Logease/__init__.py
@@ -1,10 +1,27 @@
 from flask import Flask
-from os import path
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
 from Logease.config import Config
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from Logease.users.routes import users
     from Logease.main.routes import main
+    app.register_blueprint(users)
     app.register_blueprint(main)
+
     return app
